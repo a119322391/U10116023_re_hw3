425cddd201656543198b63abafa4ce0d9eb8d82d
shawn <a1193223911@gmail.com>
2014-03-25 13:37:41 +0800

    Update BMI.java
    最後版本
    扔然不會用github  and  eclipse
    所以版本很少

--- BMI/src/BMI.java
+++ BMI/src/BMI.java
@@ -1,4 +1,83 @@
-public class BMI {
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
+public class BMICalculator extends JFrame {
+  // Create text fields for interest rate, years
+  // loan amount, monthly payment, and total payment
+  private JTextField jtfNAME = new JTextField();
+  private JTextField jtfAGE = new JTextField();
+  private JTextField jtfHEIGHT = new JTextField();
+  private JTextField jtfWEIGHT = new JTextField();
+  private JTextField jtfBMI = new JTextField();
+  private JTextField jtfCONDITION = new JTextField();
+  // Create a Compute Payment button
+  private JButton jbtBMI = new JButton("GO");
+
+  public BMICalculator() {
+    // Panel p1 to hold labels and text fields
+    JPanel p1 = new JPanel(new GridLayout(6, 2));
+    p1.add(new JLabel("NAME"));
+    p1.add(jtfNAME);
+    p1.add(new JLabel("AGE"));
+    p1.add(jtfAGE);
+    p1.add(new JLabel("HEIGHT"));
+    p1.add(jtfHEIGHT);
+    p1.add(new JLabel("WEIGHT"));
+    p1.add(jtfWEIGHT);
+    p1.add(new JLabel("BMI"));
+    p1.add(jtfBMI);
+    p1.add(new JLabel("CONDITION"));
+    p1.add(jtfCONDITION);
+    p1.setBorder(new
+      TitledBorder("Enter NAME AGE HEIGHT WEIGHT"));
+
+    // Panel p2 to hold the button
+    JPanel p2 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    p2.add(jbtBMI);
+
+    // Add the panels to the frame
+    add(p1, BorderLayout.CENTER);
+    add(p2, BorderLayout.SOUTH);
+
+    // Register listener
+    jbtBMI.addActionListener(new ButtonListener());
+  }
+
+  /** Handle the Compute Payment button */
+  private class ButtonListener implements ActionListener {
+    @Override 
+    public void actionPerformed(ActionEvent e) {
+      // Get values from text fields
+      String name = jtfNAME.getText();
+      int age = Integer.parseInt(jtfAGE.getText());
+      double  height=
+        Double.parseDouble(jtfHEIGHT.getText());
+      double  weight=
+    	        Double.parseDouble(jtfWEIGHT.getText());
+      // Create a loan object
+      BMI MYBMI = new BMI(name, age, weight, height);
+
+      // Display monthly payment and total payment
+      jtfBMI.setText(String.format("%.2f", MYBMI.getBMI()));
+      jtfCONDITION.setText(String.format("%s",
+    		  MYBMI.getStatus()));
+    }
+  }
+  
+  public static void main(String[] args) {
+    BMICalculator frame = new BMICalculator();
+    frame.setSize(400,400);
+    frame.setTitle("LoanCalculator");
+    frame.setLocationRelativeTo(null); // Center the frame
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+}
+
+class BMI {
 		  private String name;
 		  private int age;
 		  private double weight; // in pounds
